58c110ce64cffd6c1d6ac0b69cc8c9e3e3c55b9d
galactic_ <102072394+MushyToast@users.noreply.github.com>
2022-12-18 12:33:04 -0800

    Create config.lua

---
+++ src/config.lua
@@ -0,0 +1,6 @@
+--MODULE SCRIPT, PARENT = MAIN.LUA
+
+return {
+	["Key"] = 69, --input key here, could be a string or a number, or really anything except bool
+	["kickOnIncorrectKey"] = true, --if service attempts to be accessed by client but inputs an incorrect key, then kick the player
+}
